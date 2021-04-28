9446b0e6a26592d2c04c7c470430e6a3d4675814
Miguel Amores <migueamores@gmail.com>
2021-04-28 10:32:27 -0500

    Create header component

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+import Header from './components/Header';
 
 export default function App() {
+  const image = require('./assets/bg.png');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <Header />
+        <Text>Open up App.js to start working on your app!</Text>
+      </ImageBackground>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +23,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  image: {
+    flex: 1,
+    padding: '20px',
+    resizeMode: 'cover',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%'
+  }
 });
---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.profile} source={require('../assets/profile.png')} />
+      <Text style={styles.name}>Lottie Curtis</Text>
+      <Text style={styles.description}>You have 3 Products</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  profile: {
+    width: '56px',
+    height: '56px'
+  },
+  name: {
+    fontSize: '18px',
+    lineHeight: '20px',
+    letterSpacing: '0.5px',
+    color: '#fff',
+    margin: '10px'
+  },
+  description: {
+    fontSize: '14px',
+    lineHeight: '20px',
+    letterSpacing: '0.39px',
+    color: '#000',
+    backgroundColor: '#fff',
+    borderRadius: '15px',
+    paddingVertical: '3px',
+    paddingHorizontal: '10px'
+  }
+});
+
+export default Header;
